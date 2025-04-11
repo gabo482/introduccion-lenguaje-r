144159dcf4f55ce9c77447a527557ecdc0e481e6
gabo482 <vcruztesci@gmail.com>
2025-04-11 13:46:53 -0600

    Se agrega script de matrices

---
+++ 7-Matrices/creando-matrices.R
@@ -0,0 +1,16 @@
+help("matrix")
+m <- matrix( 1:20, nrow =2)
+m
+m <- matrix( 1:20, ncol =2)
+m
+m*2
+vector <- c(1:4)
+vector
+m1 <- matrix( vector, ncol =2)
+m1
+m2 <- matrix( vector+1, ncol =2)
+m1
+m2
+m1+m2
+m1-m2
+m2-m1
