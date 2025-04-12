d82572a0b06632bb649f01755771367504d23245
gabo482 <vcruztesci@gmail.com>
2025-04-11 18:45:46 -0600

    Se corrige ejercicio con matrices

--- 7-Matrices/Ejercicio 1.R
+++ 7-Matrices/Ejercicio 1.R
@@ -13,65 +13,33 @@
 ##################################################
 
 
-help("matrix")
-# Como crear una matriz de 2 columnas
-m <- matrix( 1:20, ncol =2)
-m
-# Como efectuar operaciones aritmeticas de una matriz
-m*2
-m+2
-m-2
-
-#Como crer una matriz a partir de un vector
-vector <- c(1:4)
-vector
-
-# Se crea matriz  m1
-m1 <- matrix( vector, ncol =2)
-m1
-# Se crea matriz  m2
-m2 <- matrix( vector+1, ncol =2)
-m2
-#Operaciones aritmeticas con m1 y m2
-m1+m2
-m1-m2
-m2-m1
-
 #Ejercicio
-v
-ventas_prod_A <-ventas[c(1:4)]
-ventas_prod_A <-ventas[c(1:4)]+15
-ventas_prod_A <-ventas[c(1:4)]-25
-ventas_prod_A <-ventas[c(1:4)]
-ventas_prod_B <-ventas[c(1:4)]+15
-ventas_prod_C <-ventas[c(1:4)]-25
+ventas<-c(120,135,80,110)
+
+
+ventas_prod_A <-c( 95, 110,  55,  85)
+ventas_prod_B <-c( 135 , 150,  95, 125)
+ventas_prod_C <-c( 93, 108,  53,  83)
 ventas_prod_A
 ventas_prod_B
 ventas_prod_C
-help ("matrix")
-m <- as.matrix(ventas_prod_A)
-m
-m <- as.matrix(ventas_prod_A,ventas_prod_B)
-m
-vector <- c(ventas_prod_A,ventas_prod_B,ventas_prod_C)
-vector
-m <-matrix(data= vector, nrow =3 )
-m
-ventas_prod_A
+
+
 m <-matrix(data= vector, nrow =4 )
 m
+
 row_names<- c("ENE","FEB","MZO","ABR")
 row_names
 col_names <- c("Producto A","Producto B","Producto C")
 col_names
 dimnames(m)<- list(row_names,col_names)
 m
-m[,1]
-total_v_prod_a <-m[,1]
-total_v_prod_a
+
 total_v_prod_a <-sum(m[,1])
 total_v_prod_a
 total_v_prod_b <-sum(m[,2])
 total_v_prod_b
 total_v_prod_c <-sum(m[,3])
 total_v_prod_c
+
+
